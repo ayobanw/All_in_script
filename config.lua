faac935d0457694fafbde7fc24abb374ae0e30dc
ANW®©™✓ <carpkingrc1@gmail.com>
2025-07-03 04:24:24 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,2 +1,2 @@
 -- Config (boleh diubah jika repo atau URL berubah)
-REPO_URL = "https://raw.githubusercontent.com/ayobanw/keys.txt/SC ALL MENU NEW 4.9.2.lua/keys.txt"
+REPO_URL = "https://raw.githubusercontent.com/ayobanw/keys.txt/All_in_script/keys.txt"
